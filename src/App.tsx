c20dcd55056a323069b700081cd0aa419e463a11
brahim <boulhend.brahim@gmail.com>
2022-01-27 00:09:09 +0100

    Add keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Board from "./components/Board";
-
+import Keyboard from "./components/Keybord";
 function App() {
   return (
-    <div className="w-1/2 mx-auto mt-16 flex justify-center">
+    <div className="w-1/2 mx-auto mt-16 flex flex-col items-center justify-center pb-10">
       <Board />
+      <Keyboard />
     </div>
   );
 }
---
+++ src/components/Keybord.tsx
@@ -0,0 +1,23 @@
+import { lettersList } from "../data/letters-list";
+
+const Keyboard = () => {
+  return (
+    <div className="flex justify-center flex-wrap my-10 w-[600px] h-[198px]">
+      {lettersList.map((letter) => (
+        <button
+          className={`cursor-pointer flex justify-center items-center  ${
+            letter === "Enter" || letter === "Backspace" ? "w-40" : "w-14"
+          } h-14 rounded-md bg-gray-300 text-black text-lg font-bold m-1`}
+          onClick={(event) =>
+            console.log((event.target as HTMLInputElement).value)
+          }
+          value={letter}
+        >
+          {letter}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Keyboard;
