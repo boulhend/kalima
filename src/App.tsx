fce697eebf9e7d5dac315ab9ce9eab8f5a660fa1
brahim <boulhend.brahim@gmail.com>
2022-01-27 19:16:45 +0100

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Board from "./components/Board";
-import Keyboard from "./components/Keybord";
+import Header from "./components/Header";
 function App() {
   return (
-    <div className="w-1/2 mx-auto mt-16 flex flex-col items-center justify-center pb-10">
+    <div className="w-1/2 mx-auto flex flex-col items-center justify-center pb-10">
+      <Header />
       <Board />
     </div>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="mb-6 border-b-2 w-[700px] text-center py-2 ">
+      <h1 className="text-4xl">كلمة</h1>
+    </div>
+  );
+};
+
+export default Header;
